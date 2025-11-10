8138b691c10e33efa749247304221c237110d4bf
Rohit Thimaya <rohitthimaya@Rohits-MacBook-Pro.local>
2025-11-09 23:47:07 -0800

    config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const apiPort = process.env.API_PORT || process.env.PORT || '3000'
-
 export default defineConfig({
   plugins: [react()],
   server: {
     port: Number(process.env.CLIENT_PORT || 5173),
-    proxy: {
-      '/api': {
-        target: `http://localhost:${apiPort}`,
-        changeOrigin: true
-      }
-    }
+    proxy: process.env.NODE_ENV === 'development'
+      ? {
+          '/api': {
+            target: `http://localhost:3000`,
+            changeOrigin: true
+          }
+        }
+      : undefined
   }
 })
-
